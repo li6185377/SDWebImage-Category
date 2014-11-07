2565d2b7868b34c4f04e5adcf7491408fccc1dc4
ljh2 <li6185377@163.com>
2014-11-07 12:41:50 +0800

    Xcode 6.1 builder

--- SDWebImage/SDWebImageDownloaderOperation.m
+++ SDWebImage/SDWebImageDownloaderOperation.m
@@ -34,6 +34,8 @@
     size_t width, height;
     BOOL responseFromCached;
 }
+@synthesize executing = _executing;
+@synthesize finished = _finished;
 
 - (id)initWithRequest:(NSURLRequest *)request options:(SDWebImageDownloaderOptions)options progress:(void (^)(NSInteger, NSInteger))progressBlock completed:(void (^)(UIImage *, NSData *, NSError *, BOOL))completedBlock cancelled:(void (^)())cancelBlock {
     if ((self = [super init])) {
